f98c0867fc5e9599c475737740d40a2350123c84
亨睿 <henry.lx@alibaba-inc.com>
2021-12-19 13:00:59 +0800

    feat: add typings

---
+++ src/typings/EthereumProvider.d.ts
@@ -0,0 +1,22 @@
+interface RequestArguments {
+  method: string;
+  params?: unknown[] | object;
+}
+
+interface EthereumProvider {
+  /**
+   * Returns true if the provider is connected to the current chain, and false otherwise.
+   *
+   * @remarks
+   * Note that this method has nothing to do with the user's accounts.
+   *
+   * You may often encounter the word "connected" in reference to whether a web3 site can access the user's accounts. In the provider interface, however, "connected" and "disconnected" refer to whether the provider can make RPC requests to the current chain.
+   */
+  isConnected(): boolean;
+
+  /**
+   * Use request to submit RPC requests to Ethereum via MetaMask. It returns a Promise that resolves to the result of the RPC method call.
+   * @param args A `RequestArguments` object.
+   */
+  request(args: RequestArguments): Promise<any>;
+}
