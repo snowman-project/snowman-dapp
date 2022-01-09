58eb0b14f0142ced244bd88c97f4d969585a5958
亨睿 <henry.lx@alibaba-inc.com>
2022-01-09 23:32:36 +0800

    chore: add useAccount()

---
+++ src/hooks/use-account.ts
@@ -0,0 +1,32 @@
+import { useEthers } from '@usedapp/core';
+import { useEffect, useState } from 'react';
+
+export function useAccount() {
+  const { account } = useEthers();
+  const [nameInState, setNameInState] = useState<string | null>('Anonymous');
+
+  useEffect(() => {
+    if (account) {
+      let name = null;
+      const nameStored = localStorage.getItem(storageKey);
+      if (nameStored) {
+        name = nameStored;
+      }
+      setNameInState(name);
+    }
+  }, [account]);
+
+  const storageKey = `snowman.account.${account}.name`;
+
+  const changeAccountName = (name: string | null) => {
+    if (!account) return;
+    if (name === null) {
+      localStorage.removeItem(storageKey);
+    } else {
+      localStorage.setItem(storageKey, name);
+    }
+    setNameInState(name);
+  };
+
+  return { account, accountName: nameInState, changeAccountName };
+}
