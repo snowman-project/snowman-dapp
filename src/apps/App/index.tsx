20ad1aea8ad77982e7ed00cc39a10c5333fb7218
亨睿 <henry.lx@alibaba-inc.com>
2022-01-09 23:34:10 +0800

    feat: add MyProfilePage

--- src/apps/App/index.tsx
+++ src/apps/App/index.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AppHeader } from '@/components/AppHeader';
 
 import { LandingPage } from '@/pages/LandingPage';
+import { MyProfilePage } from '@/pages/MyProfilePage';
 
 import useDAppConfig from '../../use-dapp.config';
 
@@ -17,6 +18,7 @@ export function App() {
           <AppHeader />
           <Routes>
             <Route index element={<LandingPage />} />
+            <Route path="/my/profile" element={<MyProfilePage />} />
           </Routes>
         </BrowserRouter>
       </div>
---
+++ src/pages/MyProfilePage/index.tsx
@@ -0,0 +1,103 @@
+import { shortenAddress } from '@usedapp/core';
+import Toast from 'antd-mobile/es/components/toast';
+import Input from 'antd-mobile/es/components/input';
+import { CheckCircleOutline, EditSOutline } from 'antd-mobile-icons';
+import { useMemo, useRef, useState } from 'react';
+
+import { useAccount } from '@/hooks';
+
+import styles from './index.module.less';
+
+const QR_CODE_IMAGE_SIZE = 180;
+
+export function MyProfilePage() {
+  const { account, accountName, changeAccountName } = useAccount();
+  const [editing, setEditing] = useState(false);
+  const [editingAccountName, setEditingAccountName] = useState('');
+  const inputRef = useRef<any>(null);
+  const qrCodeImageURL = useMemo(() => {
+    if (account) {
+      const url = new URL(
+        `https://api.qrserver.com/v1/create-qr-code/?size=${QR_CODE_IMAGE_SIZE}x${QR_CODE_IMAGE_SIZE}`
+      );
+      url.searchParams.set('data', `ethereum:${account}`);
+      return url;
+    }
+    return null;
+  }, [account]);
+
+  if (account) {
+    const handleCopy = () => {
+      navigator.clipboard.writeText(account);
+      Toast.show('Copied to clipboard');
+    };
+    const handleEdit = () => {
+      setEditingAccountName(accountName ?? '');
+      setEditing(true);
+      setTimeout(() => {
+        inputRef.current?.focus();
+      });
+    };
+    const handleChangeAccountName = () => {
+      const length = editingAccountName.trim().length;
+      if (length) {
+        if (length <= 64) {
+          changeAccountName(editingAccountName.trim());
+          setEditing(false);
+        } else {
+          Toast.show('The nick name is too long. Max length is 64.');
+        }
+      } else {
+        changeAccountName(null);
+      }
+    };
+
+    return (
+      <div className={styles.container}>
+        <div className={styles.centered}>
+          <div className={styles.name}>
+            {editing ? (
+              <>
+                <Input
+                  ref={inputRef}
+                  className={styles.nameInput}
+                  placeholder="Nick name"
+                  value={editingAccountName}
+                  onChange={(value) => {
+                    setEditingAccountName(value);
+                  }}
+                  onEnterPress={handleChangeAccountName}
+                />
+                <CheckCircleOutline onClick={handleChangeAccountName} />
+              </>
+            ) : (
+              <>
+                <div className={styles.readonly}>
+                  {accountName ?? shortenAddress(account)}
+                </div>
+                <EditSOutline
+                  className={styles.editButton}
+                  onClick={handleEdit}
+                />
+              </>
+            )}
+          </div>
+          <div className={styles.qrCode}>
+            {qrCodeImageURL && (
+              <img
+                width={QR_CODE_IMAGE_SIZE}
+                height={QR_CODE_IMAGE_SIZE}
+                src={qrCodeImageURL.toString()}
+              />
+            )}
+          </div>
+          <div className={styles.address} onClick={handleCopy}>
+            {account}
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
