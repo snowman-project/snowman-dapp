5df24d2d22bbbd19dc828e0adaf3193592250fcc
亨睿 <henry.lx@alibaba-inc.com>
2022-01-10 13:46:04 +0800

    feat: add event emitter

--- src/hooks/use-account.ts
+++ src/hooks/use-account.ts
@@ -1,9 +1,24 @@
 import { useEthers } from '@usedapp/core';
+import EventEmitter from 'eventemitter3';
 import { useEffect, useState } from 'react';
+
+const accountEvent = new EventEmitter();
 
 export function useAccount() {
   const { account } = useEthers();
   const [nameInState, setNameInState] = useState<string | null>('Anonymous');
+
+  useEffect(() => {
+    const handleAccountNickNameChanged = (nickName: string) => {
+      setNameInState(nickName);
+    };
+    accountEvent.on('accountNickNameChanged', (nickName) => {
+      setNameInState(nickName);
+    });
+    return () => {
+      accountEvent.off('accountNickNameChanged', handleAccountNickNameChanged);
+    };
+  }, []);
 
   useEffect(() => {
     if (account) {
@@ -26,6 +41,7 @@ export function useAccount() {
       localStorage.setItem(storageKey, name);
     }
     setNameInState(name);
+    accountEvent.emit('accountNickNameChanged', name);
   };
 
   return {
