720df9e34ed5ded642539941e88cccb32e252866
亨睿 <henry.lx@alibaba-inc.com>
2022-01-10 13:23:56 +0800

    feat: add account info

--- src/components/AppSideBar/index.tsx
+++ src/components/AppSideBar/index.tsx
@@ -1,4 +1,12 @@
+import { shortenAddress } from '@usedapp/core';
+import List from 'antd-mobile/es/components/list';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
+
 import { AppMenu } from '../AppMenu';
+
+import { useAccount } from '@/hooks';
+
 import styles from './index.module.less';
 
 export interface AppSideBarProps {
@@ -6,9 +14,45 @@ export interface AppSideBarProps {
 }
 
 export function AppSideBar({ onClose }: AppSideBarProps) {
-  return (
-    <div className={styles.container}>
-      <AppMenu onClose={onClose} />
-    </div>
-  );
+  const { account, accountNickName: accountNickName } = useAccount();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (account) {
+      (window as any).jdenticon();
+    }
+  }, [account]);
+  if (account) {
+    const shortenAccount = shortenAddress(account);
+    const handleJumpToProfile = () => {
+      navigate('/my/profile');
+      onClose && onClose();
+    };
+    return (
+      <div className={styles.container}>
+        <List>
+          <List.Item>
+            <div className={styles.brandName}>雪人理财</div>
+          </List.Item>
+          <List.Item onClick={handleJumpToProfile}>
+            <div className={styles.avatarAndAccountInfo}>
+              <div className={styles.avatar}>
+                <svg
+                  className={styles.avatarSVG}
+                  data-jdenticon-hash={account?.substr(2)}
+                ></svg>
+              </div>
+              <div className={styles.accountInfo}>
+                <h3 className={styles.accountNickName}>
+                  {accountNickName ?? shortenAccount}
+                </h3>
+                <div className={styles.accountAddress}>{shortenAccount}</div>
+              </div>
+            </div>
+          </List.Item>
+        </List>
+        <AppMenu onClose={onClose} />
+      </div>
+    );
+  }
+  return null;
 }
