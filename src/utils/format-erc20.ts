35f4b40560e900a92ec6a8b1ba533a00a3addbbe
亨睿 <henry.lx@alibaba-inc.com>
2022-01-11 20:55:37 +0800

    feat: return null instead of empty string

--- src/utils/format-erc20.ts
+++ src/utils/format-erc20.ts
@@ -4,7 +4,7 @@ import { ERC20Metadata } from '@/metadata';
 
 export function formatERC20(num: BigNumber | undefined, token: ERC20Metadata) {
   if (typeof num === 'undefined') {
-    return '';
+    return null;
   }
   return ethers.utils.formatUnits(num, token.decimals);
 }
