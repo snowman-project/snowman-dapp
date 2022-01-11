0dbd9b35e51f2c3243087521fd881388b8666e0b
亨睿 <henry.lx@alibaba-inc.com>
2022-01-11 13:11:36 +0800

    feat: add format-erc20

---
+++ src/utils/format-erc20.ts
@@ -0,0 +1,10 @@
+import { BigNumber, ethers } from 'ethers';
+
+import { ERC20Metadata } from '@/metadata';
+
+export function formatERC20(num: BigNumber | undefined, token: ERC20Metadata) {
+  if (typeof num === 'undefined') {
+    return '';
+  }
+  return ethers.utils.formatUnits(num, token.decimals);
+}
