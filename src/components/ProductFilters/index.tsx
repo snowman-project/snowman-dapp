e14defdebad3cad4a869a10d1af328a8ec9b44cc
Henry Li <henry1943@163.com>
2022-01-12 23:05:03 +0800

    feat: add ProductFilters

---
+++ src/components/ProductFilters/index.tsx
@@ -0,0 +1,48 @@
+import { Picker } from 'antd-mobile';
+import { PickerColumn } from 'antd-mobile/es/components/picker-view';
+import { DownFill } from 'antd-mobile-icons';
+import { useState } from 'react';
+
+import styles from './index.module.less';
+
+export {
+  PickerColumn,
+  PickerColumnItem,
+} from 'antd-mobile/es/components/picker-view';
+
+export interface ProductFiltersProps {
+  children: React.ReactElement<ProductFilterProps>[];
+}
+
+export function ProductFilters({ children }: ProductFiltersProps) {
+  const [pickerVisible, setPickerVisible] = useState(false);
+
+  const columns = children.map((child) => child.props.options);
+  return (
+    <>
+      <div className={styles.container} onClick={() => setPickerVisible(true)}>
+        {children}
+      </div>
+      <Picker
+        columns={columns}
+        visible={pickerVisible}
+        confirmText="筛选"
+        onClose={() => setPickerVisible(false)}
+      />
+    </>
+  );
+}
+
+export interface ProductFilterProps {
+  name: string;
+  options: PickerColumn;
+}
+
+export function ProductFilter({ name }: ProductFilterProps) {
+  return (
+    <div className={styles.filter}>
+      <span className={styles.filterName}>{name}</span>
+      <DownFill fontSize="0.66rem" />
+    </div>
+  );
+}
